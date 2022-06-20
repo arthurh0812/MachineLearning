daa795105ad5d83824c7eb8371a089fc621940c8
arthurh0812 <arthurh1204@gmail.com>
2022-06-20 23:01:38 +0200

    Comparison, Logic, booleans and conditional code execution

---
+++ Chapter_2/logic.py
@@ -0,0 +1,23 @@
+i_am_broke = True
+
+if i_am_broke:
+    print("I need money")
+else:
+    print("I'm chilling")
+
+my_bank_account = 2000
+i_am_broke = my_bank_account <= 10
+
+if i_am_broke:
+    print("I need money")
+else:
+    print("I'm chilling")
+    
+age = 17
+
+if age < 18:
+    print("I am a child")
+elif age < 66:
+    print("I am an adult")
+else:
+    print("I am an elder")
