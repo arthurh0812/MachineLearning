9549462c89f0dcc57e9f3b3c25c66c542e6c7c1d
arthurh0812 <arthurh1204@gmail.com>
2022-06-20 22:43:00 +0200

    Revision of variables and data types

---
+++ Chapter_2/variables.py
@@ -0,0 +1,12 @@
+name = "Arthur Hennig" # str
+place = "Königstein im Taunus"
+age = 17 # int
+height = 1.76 # float
+weight = 69
+has_drivers_license = False # bool
+
+print(name + " lives in " + place + " and is " + str(age) + " years old. He is " + str(height) + " meters tall and weighs " + str(weight) + " kilograms.")
+
+body_info = str(height*100) + "cm;" + str(weight) + "kg"
+
+print(body_info)
