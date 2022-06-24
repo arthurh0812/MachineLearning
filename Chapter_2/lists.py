db3e2d36a55a87dc1ca333da79e8ea590f38aa5d
arthurh0812 <arthurh1204@gmail.com>
2022-06-24 18:10:02 +0200

    Working with lists in Python

---
+++ Chapter_2/lists.py
@@ -0,0 +1,29 @@
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+grades = [1, 2, 3]
+
+print(grades.pop())
+
+print(grades)
+
+grades.append(5)
+grades.insert(len(grades), 5) # equivalent to grades.append
+
+print(grades)
+
+grades.insert(3, 10)
+
+print(grades)
+
+grades.sort()
+
+print(grades)
+
+weekdays.sort()
+
+print(weekdays)
+
+weekdays.pop(3)
+
+print(weekdays)
+
